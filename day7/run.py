a8805f7a46c38ba5e595c2ad48b88a19cec7681c
Wiebe Kooistra <wiebek@Wiebes-MacBook-Pro.local>
2024-12-18 11:14:51 +0100

    adding day7 part 2

--- day7/run.py
+++ day7/run.py
@@ -2,6 +2,19 @@
 
 from itertools import product
 
+def calculate_results(l: list) -> int:
+    total = int(l[0])
+    for i in range(len(l[1:])):
+        if i % 2 != 0:
+            n = int(l[i + 1])
+            if l[i] == '+':
+                total += n
+            elif l[i] == '*':
+                total *= n
+            elif l[i] == "||":
+                total = int(f"{total}{l[i+1]}")
+    return total
+
 def generate_permutations(numbers):
     operators = ["+", "*"]
     num_slots = len(numbers) - 1  # Number of operator slots
@@ -15,7 +28,25 @@ def generate_permutations(numbers):
             result.append(num)
             if i < len(operator_combo):
                 result.append(operator_combo[i])
-        permutations.append(eval("".join(result)))
+        # permutations.append(eval("".join(result)))
+        permutations.append(result)
+    return permutations
+
+def generate_permutations_p2(numbers):
+    operators = ["+", "*", "||"]
+    num_slots = len(numbers) - 1  # Number of operator slots
+    all_operator_combinations = product(operators, repeat=num_slots)
+    
+    permutations = []
+    for operator_combo in all_operator_combinations:
+        # Interleave numbers and operators
+        result = []
+        for i, num in enumerate(numbers):
+            result.append(num)
+            if i < len(operator_combo):
+                result.append(operator_combo[i])
+        # permutations.append(eval("".join(result)))
+        permutations.append(result)
     return permutations
 
 
@@ -37,7 +68,7 @@ def main(file_name):
         two cause the right side to match the test value: 81 + 40 * 27 and 81 * 40 + 27 both equal 3267 (when evaluated left-to-right)!
     292: 11 6 16 20 can be solved in exactly one way: 11 + 6 * 16 + 20.
     """
-    results = {}
+    results_p1 = 0
     with open(file=file_name, mode='r') as f:
         for line in f:
             r, e = line.strip().split(":")
@@ -45,12 +76,28 @@ def main(file_name):
             m = ["+", "*"]
 
             permutations = generate_permutations(e)
-            print(permutations)
-            print(r)
-            if r in permutations:
-                print(r, e)
+            for p in permutations:
+                if int(r) == calculate_results(p):
+                    results_p1 += int(r)
+                    break
 
+    print(f"Part1: {results_p1}")
+
+    ## Part 1
+    results_p2 = 0
+    with open(file=file_name, mode='r') as f:
+        for line in f:
+            r, e = line.strip().split(":")
+            e = e.strip().split(" ")
+
+            permutations = generate_permutations_p2(e)
+            for p in permutations:
+                if int(r) == calculate_results(p):
+                    results_p2 += int(r)
+                    break
+    print(f"Part2: {results_p2}")
+    
 
 if __name__ == "__main__":
-    file_name = "test7.txt"
+    file_name = "data7.txt"
     main(file_name=file_name)
